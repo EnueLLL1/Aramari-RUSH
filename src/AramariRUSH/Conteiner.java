ff42c5c6046c63885ca30a616dfd54f860cc8086
marktom <bigmarkto5916@gmail.com>
2025-09-05 15:53:12 -0300

    Fix resoluçao da tela

--- src/AramariRUSH/Conteiner.java
+++ src/AramariRUSH/Conteiner.java
@@ -1,21 +1,27 @@
 package AramariRUSH;
-import Modelo.Fase1;
 
+import Modelo.Fase1;
 import javax.swing.JFrame;
 
 public class Conteiner extends JFrame{
 
+    
     public Conteiner() {
         add(new Fase1());
-        setTitle("Aramari RUSH");
         setSize(1920, 1080);
+        setTitle("Aramari RUSH");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        this.setResizable(true);
+        setResizable(true);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setVisible(true);
+
+        Fase1 menu = new Fase1();
+        setContentPane(menu);
     }
     public static void main (String[] args){
-        new Conteiner();
+
+         javax.swing.SwingUtilities.invokeLater(Conteiner::new);
 
     }
 }
--- src/Modelo/Fase1.java
+++ src/Modelo/Fase1.java
@@ -1,8 +1,7 @@
 package Modelo;
 
-import javax.swing.*;
 import java.awt.*;
-
+import javax.swing.*;
 public class Fase1 extends JPanel {
 
     private Image fundoTela;
@@ -11,9 +10,49 @@ public class Fase1 extends JPanel {
         ImageIcon pegaimagem = new ImageIcon("src//res//ChatGPT Image 29 de ago. de 2025, 19_14_58.png");
         fundoTela = pegaimagem.getImage();
     }
-    public void paint(Graphics g){
-        Graphics2D graficos = (Graphics2D) g;
-        graficos.drawImage(fundoTela, 0, 0, null);
-        g.dispose();
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        if (fundoTela == null) {
+            
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.WHITE);
+            g.drawString("Imagem do menu não encontrada", 20, 20);
+            return;
+        }
+
+        Graphics2D g2 = (Graphics2D) g.create();
+        
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+        int panelW = getWidth();
+        int panelH = getHeight();
+
+        
+        int imgW = fundoTela.getWidth(this);
+        int imgH = fundoTela.getHeight(this);
+
+        double imgAspect = (double) imgW / imgH;
+        double panelAspect = (double) panelW / panelH;
+
+        int drawW, drawH;
+
+         if (panelAspect > imgAspect) {
+            
+            drawH = panelH;
+            drawW = (int) (drawH * imgAspect);
+        } else {
+            
+            drawW = panelW;
+            drawH = (int) (drawW / imgAspect);
+        }
+
+        int x = (panelW - drawW) / 2;
+        int y = (panelH - drawH) / 2;
+
+        g2.drawImage(fundoTela, x, y, drawW, drawH, this);
+        g2.dispose();
     }
 }
